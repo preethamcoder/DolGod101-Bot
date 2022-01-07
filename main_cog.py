a57bed9bcaa7273f4b166466ac780a62c6b98ea4
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2022-01-06 22:29:19 -0500

    Added the regular roast feature

--- main_cog.py
+++ main_cog.py
@@ -7,7 +7,7 @@ class main_cog(commands.Cog):
         self.word1 = ["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy",	"gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous",	"warped", "wayward", "weedy", "yeasty"]
         self.word2 = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen",	"fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered",	"motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
         self.word3 = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard",	"harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
-        self.roasts = ["I'd offer you some gum but your smiles got plenty of it.", "Repeat after me: semen is not hair gel.", "Your body fat is about as evenly distributed as wealth in the US economy." "You are proof evolution can go in reverse." "I wasn't born with enough middle fingers to let you know how I feel about you.", "Your birth certificate is an apology letter from the condom factory."]
+        self.roasts = ["I guess you prove that even god makes mistakes sometimes.", "I bet your brain feels as good as new, seeing that you never use it.", "I'd offer you some gum but your smile's got plenty of it.", "Repeat after me: semen is not hair gel.", "Your body fat is about as evenly distributed as wealth in the US economy.", "You are proof evolution can go in reverse.", "I wasn't born with enough middle fingers to let you know how I feel about you.", "Your birth certificate is an apology letter from the condom factory.", "You have the perfect face for radio.", "You're not completely useless, you can always serve as a bad example."]
         self.help_message = """
 ```
 General commands:
@@ -22,13 +22,12 @@ Music commands:
 >dc - leaves the voice channel and stops playing all the songs
 Fun section:
 >sh_roast - light-heartedly roasts the tagged person in Shakespearnean English. Caution: THIS CAN BE EXTREMELY OFFENSIVE 😲😲
->roast - randomly roasts the tagged person in *REGULAR* English
+>roast - randomly roasts the tagged person in *REGULAR* English; the dataset is realtively small now, so there aren't many roasts out there
 ```
 """
         self.text_channel_list = []
 
     #some debug info so that we know the bot has started          
-
     @commands.command(name="help", help="Displays all the available commands")
     async def help(self, ctx):
         await ctx.send(self.help_message)
@@ -59,4 +58,16 @@ Fun section:
                 w2_ind = random.randint(0, len(self.word2)-1)
                 w3_ind = random.randint(0, len(self.word3)-1)
                 mes += f"{members[ind].mention}, thou " + self.word1[w1_ind] + " " + self.word2[w2_ind] + " " + self.word3[w3_ind] + '\n'
+            await ctx.send(mes)
+    
+    @commands.command(name="roast", help="Roast the tagged user(s) in REGULAR English")
+    async def roast(self, ctx):
+        members = ctx.message.mentions
+        if(len(members) == 0):
+            await ctx.send(f"I don't know who to roast, {ctx.author.mention}")
+        else:
+            mes = ''
+            for ind in range(len(members)):
+                r_ind = random.randint(0, len(self.roasts)-1)
+                mes += f"{members[ind].mention} " + self.roasts[r_ind] + '\n\n'
             await ctx.send(mes)
